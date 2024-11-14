ddff167973957315f9caffd01d247c4d9c25afda
e28121996 <e28121996@users.noreply.github.com>
2024-11-14 04:24:38 +0000

    ✅ Ready to clone and code.

--- .github/rename_project.sh
+++ .github/rename_project.sh
@@ -16,10 +16,10 @@ echo "Description: $description";
 
 echo "Renaming project..."
 
-original_author="author_name"
-original_name="project_name"
-original_urlname="project_urlname"
-original_description="project_description"
+original_author="e28121996"
+original_name="adbot"
+original_urlname="AdBot"
+original_description="Awesome adbot created by e28121996"
 # for filename in $(find . -name "*.*") 
 for filename in $(git ls-files) 
 do
@@ -30,7 +30,7 @@ do
     echo "Renamed $filename"
 done
 
-mv project_name $name
+mv adbot $name
 
 # This command runs only once on GHA!
 rm -rf .github/template.yml
